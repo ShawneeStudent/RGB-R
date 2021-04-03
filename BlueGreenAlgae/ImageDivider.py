bd4f549e5c0f9492f669529a81e84fdf6fdf2c48
Nick Ludowese <Ludowese1>
2021-04-03 19:50:18 -0400

    mor python imaging stuff

--- BlueGreenAlgae/ImageDivider.py
+++ BlueGreenAlgae/ImageDivider.py
@@ -2,26 +2,28 @@ from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 
-def divide(imageArray):
-    t=0
-    newImage=[]
-    for row in imageArray:
-        pixels=[]
-        j=0
-        for pixel in row:
-            pixels.append(pixel)
+def divide(imageArray,length,width):
+    i=0
+    j=0
+    newImage= bytearray(32*32*4)
+    for row in range(length,length+32):
+        for pixel in range(width,width+32):
+            newImage[i][j]=imageArray.getpixel((row, pixel))
             j+=1
-            if j==32:
-                break
-        newImage.append(pixels)
-        t+=1
-        if t==32:
-            break
-    plt.imshow(newImage)
-    plt.show()
-i=Image.open('sans.tiff')
-iar=np.asarray(i)
+        i+=1
+    Image.frombytes("CMYK", (32, 32), bytes(newImage)).save('BankOImages/something_'+length+'_'+width+'.TIF')
+
+
+i=Image.open('something.TIF')
+iar=i.getdata()
+(w,h)=i.size
 if __name__ == "__main__":
-    plt.imshow(iar)
-    plt.show()
-    divide((iar))
+    u=0
+    v = 0
+    while u<h:
+        while v<w:
+            divide(i,u,v)
+            v+=32
+        u+=32
+
+
