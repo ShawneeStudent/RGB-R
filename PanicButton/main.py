a3d8d39b0e5ef9234ec553d6fa41c6c51da71a0c
Demetrius Reynolds <reynoldsd@mymail.shawnee.edu>
2021-04-05 03:09:36 -0400

    Final fix

--- PanicButton/main.py
+++ PanicButton/main.py
@@ -10,9 +10,11 @@ if __name__ == '__main__':
             ext = os.path.splitext(file)[-1].lower()
             if ext in extensions:
                 with open(os.path.realpath("./Convert/" +file)[:-4] + ".txt") as txt:
-                    out = txt.readline()
-                    if(out == "Tag.SPORECYST"):
-                        out = "B"
+                    out = txt.readline().strip()
+                    if(out == "Tag.CYST"):
+                        shutil.copyfile(os.path.realpath("./Convert/" +file), os.path.realpath("./Output") + "/F" + os.path.basename(file)[:-4] + "C" + ".tif")
+                        print("cabbage")
+                        continue
                     else:
                         out = out[4]
                     shutil.copyfile(os.path.realpath("./Convert/" +file), os.path.realpath("./Output") + "/" + os.path.basename(file)[:-4] + out + ".tif")
