271bdbfdd1391fc084733f71e8957273ef17752b
Demetrius Reynolds <reynoldsd@mymail.shawnee.edu>
2021-04-05 03:01:49 -0400

    panic Fix

--- PanicButton/main.py
+++ PanicButton/main.py
@@ -10,6 +10,10 @@ if __name__ == '__main__':
             ext = os.path.splitext(file)[-1].lower()
             if ext in extensions:
                 with open(os.path.realpath("./Convert/" +file)[:-4] + ".txt") as txt:
-                    out = txt.readline()[4]
+                    out = txt.readline()
+                    if(out == "Tag.SporeCyst"):
+                        out = "B"
+                    else:
+                        out = out[4]
                     shutil.copyfile(os.path.realpath("./Convert/" +file), os.path.realpath("./Output") + "/" + os.path.basename(file)[:-4] + out + ".tif")
 
