72b33b44fd073fa96fce608ee1c483a7576e7cbe
Demetrius Reynolds <reynoldsd@mymail.shawnee.edu>
2021-04-05 02:55:17 -0400

    PanicButton

---
+++ PanicButton/main.py
@@ -0,0 +1,15 @@
+import os
+import shutil
+
+if __name__ == '__main__':
+
+    rootdir = 'Convert'
+    extensions = ('.tif')
+    for subdir, dirs, files in os.walk(rootdir):
+        for file in files:
+            ext = os.path.splitext(file)[-1].lower()
+            if ext in extensions:
+                with open(os.path.realpath("./Convert/" +file)[:-4] + ".txt") as txt:
+                    out = txt.readline()[4]
+                    shutil.copyfile(os.path.realpath("./Convert/" +file), os.path.realpath("./Output") + "/" + os.path.basename(file)[:-4] + out + ".tif")
+
